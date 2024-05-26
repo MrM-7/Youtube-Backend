897537e228b4346836ad759e189548ac1b8e1b9f
Mehul Raj <rmmehul7@gmail.com>
2024-05-26 21:31:09 +0530

    feat: Add healthcheck route and controller for API monitoring

--- src/app.js
+++ src/app.js
@@ -20,9 +20,11 @@ import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 
 // routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
